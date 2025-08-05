d046cc4783d0f9351effbdd6088b96a278f6bf86
kdeb-dotcomm <eugenedebrah07@gmail.com>
2025-08-05 16:34:11 +0000

    just finished inserting the alert text

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ import {characters} from "./data.js"
 
 let firstPassword_El = document.getElementById("firstPassword")
 let secondPassword_El = document.getElementById("secondPassword")
+const generateButton = document.getElementById("generate-btn")
+const resetButton = document.getElementById("reset-btn")
 
 
-window.generate = function() {
+generateButton.addEventListener("click", generate);
+ function generate() {
     let password_1 = ""
     let password_2 = ""
     let passwordlength = 15
@@ -18,8 +21,8 @@ window.generate = function() {
        secondPassword_El.textContent = password_2
     }
 }
-
- window.reset = function() {
+resetButton.addEventListener("click", reset);
+ function reset() {
     let password_1 = ""
     let password_2 = ""
 
@@ -29,9 +32,37 @@ window.generate = function() {
 
 firstPassword_El.addEventListener("click", function() {
     navigator.clipboard.writeText(firstPassword_El.textContent)
+    showAlert()
 });
 
 secondPassword_El.addEventListener("click", function() {
     navigator.clipboard.writeText(secondPassword_El.textContent)
+    showAlertBtn2()
 });
 
+function showAlert(message = 'copied!') {
+    const alertBox = document.createElement('div');
+    alertBox.textContent = message;
+    alertBox.className = 'copy-alert';
+    document.body.appendChild(alertBox);
+  
+    // fade out after 1 second
+    setTimeout(() => {
+      alertBox.classList.add("fade-out");
+      setTimeout(() => alertBox.remove(), 500);
+    }, 500);
+  }
+  
+  function showAlertBtn2(message = 'copied!') {
+    const alertBox = document.createElement('div');
+    alertBox.textContent = message;
+    alertBox.className = 'copy-alert-1';
+    document.body.appendChild(alertBox);
+  
+    // fade out after 1 second
+    setTimeout(() => {
+      alertBox.classList.add("fade-out");
+      setTimeout(() => alertBox.remove(), 500);
+    }, 500);
+  }
+  
